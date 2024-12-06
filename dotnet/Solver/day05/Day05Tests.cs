9a7fea748d9e19ac557e7bbacf92cedcebf856da
Johan Järvenhag <johan.jarvenhag@activesolution.se>
2024-12-06 17:14:52 +0100

    Day5Part2 WIP

--- dotnet/Solver/day05/Day05Tests.cs
+++ dotnet/Solver/day05/Day05Tests.cs
@@ -35,12 +35,32 @@ public class Day05Tests(ITestOutputHelper output)
     }
 
     [Theory]
-   // [InlineData("day05/test01.txt", 143)]
-    [InlineData("day05/MyInput.txt", 4135)]
+    // [InlineData("day05/test01.txt", 143)]
+    [InlineData("day05/MyInput.txt", 5087)]
     public void Part1(string file, int expected)
     {
         RunTest(IvarSolvePart1, file, expected);
         RunTest(SolvePart1, file, expected);
+    }
+    
+    // --- Part Two ---
+    // While the Elves get to work printing the correctly-ordered updates, you have a little time to fix the rest of them.
+    //
+    //     For each of the incorrectly-ordered updates, use the page ordering rules to put the page numbers in the right order. For the above example, here are the three incorrectly-ordered updates and their correct orderings:
+    //
+    // 75,97,47,61,53 becomes 97,75,47,61,53.
+    // 61,13,29 becomes 61,29,13.
+    // 97,13,75,29,47 becomes 97,75,47,29,13.
+    // After taking only the incorrectly-ordered updates and ordering them correctly, their middle page numbers are 47, 29, and 47. Adding these together produces 123.
+    //
+    // Find the updates which are not in the correct order. What do you get if you add up the middle page numbers after correctly ordering just those updates?
+    
+    [Theory]
+    // [InlineData("day05/test01.txt", 143)]
+    [InlineData("day05/test01.txt", 123)]
+    public void Part2(string file, int expected)
+    {
+        RunTest(SolvePart2, file, expected);
     }
 
     private int IvarSolvePart1(string[] lines)
@@ -116,34 +136,37 @@ public class Day05Tests(ITestOutputHelper output)
             var parts = rule.Split('|');
             var left = int.Parse(parts[0]);
             var right = int.Parse(parts[1]);
-            if (!rightDict.ContainsKey(left))
+            if (!rightDict.TryGetValue(left, out var rValue))
             {
-                rightDict[left] = [];
+                rValue = ([]);
+                rightDict[left] = rValue;
             }
 
-            rightDict[left].Add(right);
+            rValue.Add(right);
 
-            if (!leftDict.ContainsKey(right))
+            if (!leftDict.TryGetValue(right, out var lValue))
             {
-                leftDict[right] = [];
+                lValue = ([]);
+                leftDict[right] = lValue;
             }
 
-            leftDict[right].Add(left);
+            lValue.Add(left);
         }
 
         var okPages = new List<string>();
+        var notOkPages = new List<string>();
 
         foreach (var page in pages)
         {
             var pageOk = true;
 
             var parts = page.Split(',');
-            for (int i = 0; i < parts.Length; i++)
+            for (var i = 0; i < parts.Length; i++)
             {
                 var key = int.Parse(parts[i]);
                 // assign lefty and righty  variables the dictionary entry if exists, otherwise assign an empty list
-                var lefty = leftDict.ContainsKey(key) ? leftDict[key] : [];
-                var righty = rightDict.ContainsKey(key) ? rightDict[key] : [];
+                var lefty = leftDict.TryGetValue(key, value: out var value) ? value : [];
+                var righty = rightDict.TryGetValue(key, out var value1) ? value1 : [];
                 // check left and right side of current entry against the dictionaries
                 var leftSide = parts.Take(i);
                 var rightSide = parts.Skip(i).Take(parts.Length - i);
@@ -160,6 +183,10 @@ public class Day05Tests(ITestOutputHelper output)
             {
                 okPages.Add(page);
             }
+            else
+            {
+                notOkPages.Add(page);
+            }
         }
 
         var sum = 0;
@@ -173,5 +200,53 @@ public class Day05Tests(ITestOutputHelper output)
         }
 
         return sum;
+    }
+    
+    private int SolvePart2(string[] lines)
+    {
+        // Find the empty line
+        var emptyLine = Array.IndexOf(lines, string.Empty);
+        var rules = lines.Take(emptyLine);
+        var pages = lines.Skip(emptyLine + 1);
+        
+        // Create a dictionary with the rules
+        var rightDict = new Dictionary<int, List<int>>();
+        var leftDict = new Dictionary<int, List<int>>();
+        foreach (var rule in rules)
+        {
+            var parts = rule.Split('|');
+            var left = int.Parse(parts[0]);
+            var right = int.Parse(parts[1]);
+            if (!rightDict.TryGetValue(left, out var rValue))
+            {
+                rValue = ([]);
+                rightDict[left] = rValue;
+            }
+
+            rValue.Add(right);
+
+            if (!leftDict.TryGetValue(right, out var lValue))
+            {
+                lValue = ([]);
+                leftDict[right] = lValue;
+            }
+
+            lValue.Add(left);
+        }
+        
+        // Go through  every page in pages and create one int array of all lines in pages
+        var allPages = pages.SelectMany(page => page.Split(',').Select(int.Parse)).ToArray().Distinct();
+        // Select all keys from leftDict to int array
+        var leftKeys = leftDict.Keys.ToArray();
+        // Compare if any of the keys in leftKeys are not in allPages
+        var invalidPages = leftKeys.Where(key => !allPages.Contains(key)).ToArray();
+        
+        // 
+        
+        // Create hashmap with the rule numbers
+        // Remove from hashmap the numbers that never exists in the input
+        // Create a sorted array of the remaining numbers in hashmap according to sorting rules - Answer
+        // For each invalid page, remove all numbers from Answer that are not in the invalid page. Remaining numbers in Answer is our fixed page
+        return 0;
     }
 }
