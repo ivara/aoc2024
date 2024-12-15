f532792c22a3ee537719644714e4eb0f767a726a
Ivar Åsell <ivar.asell@activesolution.se>
2024-12-15 18:51:47 +0100

    day6 not working

--- dotnet/Solver/day06/Day06Solver.cs
+++ dotnet/Solver/day06/Day06Solver.cs
@@ -26,14 +26,129 @@ public class Day06Solver(ITestOutputHelper output)
     // [InlineData("day05/MyInput.txt", 4135)]
     public void SolvePart1(string file, int expected)
     {
-        
+        var lines = File.ReadAllLines(file);
+        var result = IvarSolvePart1(lines);
+        Assert.Equal(expected, result);
     }
-    
-    [Theory]
-    [InlineData("day06/test01.txt", 143)]
-    // [InlineData("day05/MyInput.txt", 4135)]
-    public void SolvePart2(string file, int expected)
+
+    private enum Direction
     {
+        Up,
+        Right,
+        Down,
+        Left
+    }
+
+
+    private record Position(int x, int y);
+
+    private record Board(string[,] Squares);
+    
+    private static (Position currentPosition, Direction currentDirection) GetNextPositionAndDirection(Board board,
+        Position currentPosition, Direction currentDirection, int levelsDeep = 0)
+    {
+        if (levelsDeep > 3)
+        {
+            throw new Exception("what the heeeeck happened. I'm lost");
+        }
+
+        var nextPosition = currentDirection switch
+        {
+            Direction.Up => currentPosition with { y = currentPosition.y - 1 },
+            Direction.Right => currentPosition with { x = currentPosition.x + 1 },
+            Direction.Down => currentPosition with { y = currentPosition.y + 1 },
+            Direction.Left => currentPosition with { x = currentPosition.x - 1 },
+            _ => new Position(currentPosition.x, currentPosition.y)
+        };
+
+        if(!IsInsideMap(board, nextPosition))
+            return (nextPosition, currentDirection);
         
+        if (board.Squares[nextPosition.y, nextPosition.x] != "#") 
+            return (nextPosition, currentDirection);
+        
+        // cannot go there! change direction and try moving agian
+        var nextDirection = currentDirection switch
+        {
+            Direction.Up => Direction.Right,
+            Direction.Right => Direction.Down,
+            Direction.Down => Direction.Left,
+            Direction.Left => Direction.Up,
+            _ => currentDirection
+        };
+        
+        return GetNextPositionAndDirection(board, currentPosition, nextDirection, ++levelsDeep);
+    }
+
+    private int IvarSolvePart1(string[] lines)
+    {
+        var board = CreateBoard(lines);
+        var distinctPositions = new HashSet<Position>();
+        var (currentPosition, currentDirection) = GetCurrentPositionAndDirection(board);
+
+        while (true)
+        {
+            // Add current position to list of unique positions the guard travelled
+            distinctPositions.Add(currentPosition);
+
+            // Calculate next position
+            var (nextPosition, nextDirection) = GetNextPositionAndDirection(board, currentPosition, currentDirection);
+
+            if (!IsInsideMap(board, nextPosition))
+                break;
+
+            currentPosition = nextPosition;
+        }
+
+        return distinctPositions.Count();
+    }
+
+    private static bool IsInsideMap(Board board, Position position)
+    {
+        int maxX = board.Squares.GetLength(0);
+        int maxY = board.Squares.GetLength(1);
+        
+        return position.x >= 0 && position.x < maxX
+                                  && position.y >= 0 
+                                  && position.y < maxY;
+    }
+
+    private static (Position currentPosition, Direction currentDirection) GetCurrentPositionAndDirection(
+        Board board)
+    {
+        for (var y = 0; y < board.Squares.GetLength(0); y++)
+        {
+            for (var x = 0; x < board.Squares.GetLength(1); x++)
+            {
+                switch (board.Squares[y, x])
+                {
+                    case "^":
+                        return (new Position(x, y), Direction.Up);
+                    case ">":
+                        return (new Position(x, y), Direction.Right);
+                    case "v":
+                        return (new Position(x, y), Direction.Down);
+                    case "<":
+                        return (new Position(x, y), Direction.Left);
+                }
+            }
+        }
+
+        throw new Exception("No starting position found");
+    }
+
+    private Board CreateBoard(string[] lines)
+    {
+        var squares = new string[lines.Length, lines[0].Length];
+
+        for (var y = 0; y < lines.Length; y++)
+        {
+            for (var x = 0; x < lines[y].Length; x++)
+            {
+                squares[y,x] = lines[y][x].ToString();
+            }
+        }
+      
+        return new Board(squares);
     }
 }
