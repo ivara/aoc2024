0baa54a8f1411345ff8c612d97c4fe766cd74a7e
Ivar Åsell <ivar.asell@activesolution.se>
2025-12-02 12:13:59 +0100

    Cleanup and add an improved part2 repeat-check-func

--- 2025/go/day02/solution.go
+++ 2025/go/day02/solution.go
@@ -36,7 +36,7 @@ func part2(input string) int {
 	sum := 0
 	ranges := strings.SplitSeq(input, ",")
 	for sequence := range ranges {
-		fmt.Printf("Current sequence %v\n", sequence)
+		// fmt.Printf("Current sequence %v\n", sequence)
 		parts := strings.Split(sequence, "-")
 		start, _ := strconv.Atoi(parts[0])
 		stop, _ := strconv.Atoi(parts[1])
@@ -51,12 +51,31 @@ func part2(input string) int {
 	return sum
 }
 
+func part2v2(input string) int {
+	sum := 0
+	ranges := strings.SplitSeq(input, ",")
+	for sequence := range ranges {
+		// fmt.Printf("Current sequence %v\n", sequence)
+		parts := strings.Split(sequence, "-")
+		start, _ := strconv.Atoi(parts[0])
+		stop, _ := strconv.Atoi(parts[1])
+
+		for current := start; current <= stop; current++ {
+			if isRepeating(strconv.Itoa(current)) {
+				sum += current
+			}
+		}
+	}
+
+	return sum
+}
+
 // loop over the id incrementally as a string
 // and check if it has a repeated sequence
 func isInvalidId(id int) bool {
 	stringValue := strconv.Itoa(id)
-	stop := len(stringValue)
-	for chunkSize := 1; chunkSize <= stop/2; chunkSize++ {
+	stop := len(stringValue) / 2
+	for chunkSize := 1; chunkSize <= stop; chunkSize++ {
 		if hasRepeatingChunk(stringValue, chunkSize) {
 			return true
 		}
@@ -76,4 +95,14 @@ func hasRepeatingChunk(s string, chunkSize int) bool {
 		}
 	}
 	return true
+}
+
+// Another way of checking (used in part2v2)
+func isRepeating(s string) bool {
+	for size := 1; size <= len(s)/2; size++ {
+		if len(s)%size == 0 && strings.Repeat(s[:size], len(s)/size) == s {
+			return true
+		}
+	}
+	return false
 }
--- 2025/go/day02/solution_test.go
+++ 2025/go/day02/solution_test.go
@@ -22,7 +22,27 @@ func TestIsValidId(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			got := hasRepeatingChunk(tt.whole, tt.chunkSize)
 			if got != tt.expected {
-				t.Errorf("isRepeated(%v, %v) = %v; want %v", tt.whole, tt.chunkSize, got, tt.expected)
+				t.Errorf("hasRepeatingChunk(%v, %v) = %v; want %v", tt.whole, tt.chunkSize, got, tt.expected)
+			}
+		})
+	}
+}
+
+func TestIsRepeating(t *testing.T) {
+	tests := []struct {
+		name     string
+		str      string
+		expected bool
+	}{
+		// Valid ones
+		{"Is repeating only ones", "111", true},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := isRepeating(tt.str)
+			if got != tt.expected {
+				t.Errorf("isRepeating(%v) = %v; want %v", tt.str, got, tt.expected)
 			}
 		})
 	}
@@ -38,4 +58,42 @@ func TestPart1(t *testing.T) {
 	}
 }
 
+func TestPart2(t *testing.T) {
+	input := `11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124`
+	got := part2(input)
+	want := 4174379265
+
+	if got != want {
+		t.Errorf("Got = %v; want %v", got, want)
+	}
+}
+
+func TestPart2v2(t *testing.T) {
+	input := `11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124`
+	got := part2v2(input)
+	want := 4174379265
+
+	if got != want {
+		t.Errorf("Got = %v; want %v", got, want)
+	}
+}
+
 // part1 correct answer: 24157613387
+
+// ---------------------------------------------------
+//
+//	BENCHMARKS
+//
+// ---------------------------------------------------
+func BenchmarkPart2(b *testing.B) {
+	input := `11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124`
+	for b.Loop() {
+		part2(input)
+	}
+}
+func BenchmarkPart2v2(b *testing.B) {
+	input := `11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124`
+	for b.Loop() {
+		part2v2(input)
+	}
+}
